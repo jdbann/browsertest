e60d008cf65911daa7cfa6bad90ec51b9640dd46
John Bannister <johnb@hey.com>
2021-11-06 11:41:21 +0000

    Add tests for other existing actions

--- action_test.go
+++ action_test.go
@@ -16,5 +16,14 @@ func TestActions(t *testing.T) {
 		test.WaitVisible(`div[data-target="hidden-message"]`),
 		test.Click(`button[data-action="hide-hidden-message"]`),
 		test.WaitNotVisible(`div[data-target="hidden-message"]`),
+		test.SendKeys(`select[data-action="enable-reset-select"]`, "On"),
+		test.WaitEnabled(`button[data-target="reset-select-button"]`),
+		test.Click(`button[data-action="reset-select"]`),
+		test.WaitSelected(`option[value="off"]`),
+		test.Click(`button[data-action="remove"]`),
+		test.WaitNotPresent(`button[data-target="to-remove"]`),
+		test.SendKeys(`input[name="value"]`, "An interesting piece of information."),
+		test.Submit(`input[name="value"]`),
+		test.Text(`span[data-target="submitted-value"]`).Equals("An interesting piece of information."),
 	)
 }
--- testhelper_test.go
+++ testhelper_test.go
@@ -13,7 +13,7 @@ var actionsHTML *template.Template
 func init() {
 	var err error
 
-	actionsHTML, err = template.ParseFiles("testdata/actions.html")
+	actionsHTML, err = template.ParseFiles("testdata/actions.tmpl.html")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -22,8 +22,10 @@ func init() {
 func newTestServer(t *testing.T) *httptest.Server {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/actions", func(w http.ResponseWriter, r *http.Request) {
+		value := r.PostFormValue("value")
+
 		w.WriteHeader(http.StatusOK)
-		actionsHTML.Execute(w, nil)
+		actionsHTML.Execute(w, value)
 	})
 
 	server := httptest.NewServer(mux)
